770c2dd4324b541c18230141d79f486bd307429f
anmoluppal <anmoluppal365@gmail.com>
2014-12-15 19:38:51 +0530

    Create define.py

---
+++ define.py
@@ -0,0 +1,19 @@
+
+#****************************************************************************************************************************
+
+
+VOWELS = "aeiouy"
+STEP_1a_list = ["sses", "ies", "ss", "s"]
+STEP_1a_dict = {"sses":"ss", "ies":"i", "ss":"ss", "s":""}
+STEP_1b_lsit = ["eed", "ed", "ing"]
+STEP_1b_dict = {"eed":"ee", "ed":"", "ing":""}
+STEP_3_list = ["icate", "ative", "alize", "iciti", "ical", "ful", "ness"]
+STEP_3_dict = {"icate": "ic", "ative": "", "alize": "al", "iciti": "ic", "ical": "ic", "ful": "", "ness": ""}
+STEP1B_EXT = 0
+STEP_4_list = ['al', 'ance', 'ence', 'er', 'ic', 'able', 'ible', 'ant', 'ement', 'ment', 'ent', 'ion', 'ou', 'ism', 'ate', 'iti', 'ous', 'ive', 'ize']
+STEP_4_dict = {'al':"", 'ance':"", 'ence':"", 'er':"", 'ic':"", 'able':"", 'ible':"", 'ant':"", 'ement':"", 'ment':"", 'ent':"", 'ion':"", 'ou':"", 'ism':"", 'ate':"", 'iti':"", 'ous':"", 'ive':"", 'ize':""}
+STEP2_SUFFIX = "lirnms"
+
+#**********************************************************************************************************************************
+
+
